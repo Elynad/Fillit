614e4ccd0d85baba5c53131f1692ed089ab7e2b8
mameyer <mameyer@student.42.fr>
2016-12-10 08:54:05 +0100

    Add

---
+++ ft_fillit.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fillit.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mameyer <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/10 08:47:31 by mameyer           #+#    #+#             */
+/*   Updated: 2016/12/10 08:50:52 by mameyer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// FICHIER .H DE TEST - FILLIT.H INEXISTANT
+
+#ifndef FT_FILLIT_H
+
+# define FT_FILLIT_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include "libft.h"
+
+typedef struct		s_piece
+{
+	
+}					t_piece;
+
+size_t				ft_read(char *s, char **vide);
+size_t				ft_read_next(char *s, size_t size);
+
+#endif
